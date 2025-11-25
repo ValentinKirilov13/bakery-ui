a6ee75b67900aea86d2eaaec85f630c59340e34d
Valentin Kirilov <valkilmaar@gmail.com>
2025-11-26 00:52:11 +0200

    Add static login/register

---
+++ client/src/components/register/Register.jsx
@@ -0,0 +1,96 @@
+import { Link } from "react-router";
+
+export default function Register() {
+    return (
+        <div className="grow flex flex-col justify-center p-9">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    alt="Your Company"
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                    className="mx-auto h-10 w-auto"
+                />
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
+                    Sign up
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form action="#" method="POST" className="space-y-5">
+                    <div>
+                        <label
+                            htmlFor="email"
+                            className="block text-sm/6 font-medium text-gray-100"
+                        >
+                            Email address
+                        </label>
+                        <div className="mt-1">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                autoComplete="email"
+                                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label
+                            htmlFor="password"
+                            className="block text-sm/6 font-medium text-gray-100"
+                        >
+                            Password
+                        </label>
+                        <div className="mt-1">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                autoComplete="current-password"
+                                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label
+                            htmlFor="password"
+                            className="block text-sm/6 font-medium text-gray-100"
+                        >
+                            Confirm Password
+                        </label>
+                        <div className="mt-1">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                autoComplete="current-password"
+                                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    >
+                        Sign up
+                    </button>
+                </form>
+
+                <div className="mt-10 text-center text-sm/6 text-gray-400">
+                    <span>You already have an account?</span>
+                    <Link
+                        to="/login"
+                        className="font-semibold text-indigo-400 hover:text-indigo-300 ml-2"
+                    >
+                        Sign in now
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
